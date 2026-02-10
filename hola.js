8c225e951d50ccf1ad62d659f57bb486c44ec4a4
Alexandra <alexpicos644@gmail.com>
2026-02-10 17:45:02 +0100

    Funcion restar

--- hola.js
+++ hola.js
@@ -4,10 +4,15 @@ console.log("Hola mundo");
 function sumar(a, b) {
     return a + b;
 }
+function restar(a, b) {
+    return a - b;
+}
 
 const num1 = 5;
 const num2 = 10;
 
 console.log(`La suma de ${num1} y ${num2} es: ${sumar(num1, num2)}`);
+console.log(`La resta de ${num1} y ${num2} es: ${restar(num1, num2)}`);
+
 
 
