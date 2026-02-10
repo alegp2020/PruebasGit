24126356f4553c21c5e53eeac6e79f3cda59847d
Alexandra <alexpicos644@gmail.com>
2026-02-10 17:38:32 +0100

    Funcion sumar

--- hola.js
+++ hola.js
@@ -1,1 +1,13 @@
 console.log("Hola mundo");
+
+// Función principal para sumar
+function sumar(a, b) {
+    return a + b;
+}
+
+const num1 = 5;
+const num2 = 10;
+
+console.log(`La suma de ${num1} y ${num2} es: ${sumar(num1, num2)}`);
+
+
